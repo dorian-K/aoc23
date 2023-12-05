2369ef422c80572d90cdc24c2a07c258142e782c
dork <62394594+dorian-K@users.noreply.github.com>
2023-12-05 17:12:39 +0100

    add ints func

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import re
 import numpy
 
 mint = lambda t: re.search("[0-9]+", t)
+def ints(x: str):
+    m = re.findall("[0-9]+", x)
+    m = [int(x) for x in m]
+    return m
 
 f = open("input.txt", "r")
 text = f.readlines()
